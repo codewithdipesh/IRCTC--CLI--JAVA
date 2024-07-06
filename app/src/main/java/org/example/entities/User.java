8e77abc10686c5bc55a40cc26b3ba2f5b9af5008
Dipesh Pal <codewithdipesh2022@gmail.com>
2024-07-06 21:16:41 +0530

    Init Project and added entities

---
+++ app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,13 @@
+package org.example.entities;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+    private String hashepassword;
+    private List<Ticket> ticketBooked;
+    private String UUID;
+
+
+}
